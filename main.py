dd7af24c9b88e330f8c97edeab6c4d2ee30bb0d6
abbst <abbsthepandahead@gmail.com>
2025-12-24 12:23:58 -0700

    code commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
+# tavily = TavilyClient()
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
+
+
+llm = ChatOpenAI()
+# tools = [search]
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
+def main():
+    print("Hello from langchain-search-agent!")
+    # result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in Denver on linkedin and list their details?")})
+    print(result)
+
+if __name__ == "__main__":
+    main()
